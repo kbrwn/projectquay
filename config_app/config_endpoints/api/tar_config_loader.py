0a6d94d235a50c26e08eb2fd82d545d8c2fcdbfb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 14:08:03 +0000

    Adding tarfile member sanitization to extractall()

--- config_app/config_endpoints/api/tar_config_loader.py
+++ config_app/config_endpoints/api/tar_config_loader.py
@@ -50,7 +50,26 @@ class TarConfigLoader(ApiResource):
     config_provider.new_config_dir()
     input_stream = request.stream
     with tarfile.open(mode="r|gz", fileobj=input_stream) as tar_stream:
-      tar_stream.extractall(config_provider.get_config_dir_path())
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(tar_stream, config_provider.get_config_dir_path())
 
     config_provider.create_copy_of_config_dir()
 
